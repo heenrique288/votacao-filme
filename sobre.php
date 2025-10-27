cbf98ea4b4920ceb1ee6e6ff59442db070c4c540
heenrique288 <ziguesantos082006@gmail.com>
2025-10-27 02:16:14 -0300

    Ajustando a página sobre.php

--- sobre.php
+++ sobre.php
@@ -6,9 +6,9 @@ require "login/auth.php";
     <section class="sobre-objetivo">
         <h1>Sobre Nós</h1>
         <p>
-            Nosso grupo é formado por 9 integrantes com o objetivo de desenvolver este site
+            Nosso grupo se chama "Onde os fracos não tem vez", e é formado por 9 integrantes com o objetivo de desenvolver este site
             para a indicação de filmes. Acreditamos que o cinema é uma forma poderosa de contar histórias,
-            conectar pessoas e inspirar emoções. Nosso site visa facilitar a descoberta de filmes incríveis,
+            conectar pessoas e inspirar emoções. Nosso site visa facilita a descoberta de filmes incríveis,
             permitindo que os usuários explorem, avaliem e compartilhem suas recomendações.
         </p>
     </section>
@@ -20,7 +20,7 @@ require "login/auth.php";
             <div class="card-integrante">
                 <img src="src/img/FotoHenrique.jpeg" alt="Foto do integrante 1">
                 <h3>Henrique José</h3>
-                <p>Sou desenvolver e CEO do projeto <strong>TO ZUANDO</strong>. Adoro conversar com meus amigos sobre jogos e filmes. Criei esse projeto por puro hobby, mas também com o intuito de crescer profissionalmente na área de <strong>desenvolvimento de sistemas</strong>.</p>
+                <p>Sou desenvolvedor e CEO do projeto <strong>TO ZUANDO</strong>. Adoro conversar com meus amigos sobre jogos e filmes. Criei esse projeto por puro hobby, mas também com o intuito de crescer profissionalmente na área de <strong>desenvolvimento de sistemas</strong>.</p>
                 <p><i class="fa-brands fa-instagram"></i> heenrique288</p>
                 <p><i class="bi bi-github"></i>heenrique288</p>
             </div>
@@ -28,50 +28,57 @@ require "login/auth.php";
             <div class="card-integrante">
                 <img src="src/img/FotoTheo.png" alt="Foto do integrante 2">
                 <h3>Théo Tavernard</h3>
-                <p>Breve descrição sobre o integrante 2.</p>
+                <p>Membro da formação original, estudante de cinema e editor do canal. Nas horas vagas, estou sempre vendo algum filme ou jogando alguma coisa. Ah… e um bom café puro para acompanhar.</p>
+                <p><i class="fa-brands fa-instagram"></i> theotavernard</p>
             </div>
 
             <div class="card-integrante">
                 <img src="src/img/FotoEdu.png" alt="Foto do integrante 3">
                 <h3>Eduardo Pereira</h3>
-                <p>Sou membro dos membros fundadores do grupo e faço parte da criação de temas para os videos. Sou estudante de psicologia e adoro conversar com meus amigos sobre o que assistimos e jogamos juntos de uma forma mais aprofundada. O que era apenas uma conversa entre amigos, se tornou um compartilhamento de visões e opiniões diferentes</p>
+                <p>Sou membro da formação original e faço parte da criação de temas para os videos. Sou estudante de psicologia e adoro conversar com meus amigos sobre o que assistimos e jogamos juntos de uma forma mais aprofundada. O que era apenas uma conversa entre amigos, se tornou um compartilhamento de visões e opiniões diferentes</p>
                 <p><i class="fa-brands fa-instagram"></i> edu_pereira02023</p>
             </div>
 
             <div class="card-integrante">
                 <img src="src/img/FotoJoao.png" alt="Foto do integrante 4">
                 <h3>João Victor</h3>
-                <p>Breve descrição sobre o integrante 1, seu papel no grupo ou interesse.</p>
+                <p>Sou um dos membros da formação original do grupo gosto muito de jogar video game ,filmes,séries e animes meu jogo favorito é god of war e meu anime favorito é one piece.</p>
+                <p><i class="fa-brands fa-instagram"></i> 1joaov1ctor7</p>
             </div>
 
             <div class="card-integrante">
                 <img src="src/img/FotoLuka.png" alt="Foto do integrante 5">
                 <h3>Alan Guilherme</h3>
-                <p>Breve descrição sobre o integrante 2.</p>
+                <p>Olá! Meu nome é Alan, mas meus amigos me chamam de Luka. Atualmente sou estudante de farmácia e fiz um ensino médio integrado a um curso de informática. Amo assistir desenhos e animações e meus gêneros de filmes favoritos são terror e musicais.</p>
+                <p><i class="fa-brands fa-instagram"></i> llukcxz</p>
             </div>
 
             <div class="card-integrante">
                 <img src="src/img/FotoCarlos.png" alt="Foto do integrante 6">
                 <h3>Carlos Henrique</h3>
-                <p>Breve descrição sobre o integrante 3.</p>
+                <p>Sou Carlos, um dos membros mais antigos do onde os fracos, sou fã de franquias de filmes clássicos como star wars, jurrasic park, senhor dos anéis e etc.Gosto de jogos em geral e tenho um breve conhecimento, e também sou viciado em teclado e guitarra além de participar de uma banda. Sou taxado de "criminoso" pelos outros membros (que não é tão verdade).</p>
+                <p><i class="fa-brands fa-instagram"></i> carlos_silva167</p>
             </div>
 
             <div class="card-integrante">
                 <img src="src/img/FotoPalacio.png" alt="Foto do integrante 7">
                 <h3>Guilherme Barros</h3>
-                <p>Breve descrição sobre o integrante 1, seu papel no grupo ou interesse.</p>
+                <p>Da nova formação do "Onde os Fracos Não Têm Vez", um aspirante a cinéfilo que gosta de fazer reviews longas no Letterboxd e também de ouvir músicas que quase ninguém do resto do grupo gosta.</p>
+                <p><i class="fa-brands fa-instagram"></i> barrosjgl</p>
             </div>
 
             <div class="card-integrante">
                 <img src="src/img/FotoGeo.png" alt="Foto do integrante 8">
-                <h3>Giovana Dutra</h3>
-                <p>Breve descrição sobre o integrante 2.</p>
+                <h3>Geovana Dutra</h3>
+                <p>Sou a Geovana e gosto muito de jogar basquete e ver filmes. Atualmente participo de diversas competições de basquete, mas no meu tempo livre estou disposta a colaborar e participar do projeto.</p>
+                <p><i class="fa-brands fa-instagram"></i> dutrakjf</p>
             </div>
 
             <div class="card-integrante">
                 <img src="src/img/FotoGabriel.png" alt="Foto do integrante 9">
                 <h3>Gabriel Izawa</h3>
-                <p>Breve descrição sobre o integrante 3.</p>
+                <p>Sou gabriel gosto muito de Adam Sandler, mas meu filme favorito é Cidade de Deus. Gosto de filmes de comédia e gosto bastante de séries policiais.</p>
+                <p><i class="fa-brands fa-instagram"></i> izawaxsv_</p>
             </div>
 
             <!-- Continue até os 9 -->
